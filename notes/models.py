47b493aa5c7b8022757b59ae67a8b7f0c44a6afd
mattfreire <freire1996@gmail.com>
2018-01-24 00:37:24 +0200

    Advancing the Entry Model

--- notes/models.py
+++ notes/models.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
-
 class Entry(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL) 
 	title = models.CharField(max_length=120)
 	description = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True) #The moment it's created
+	updated = models.DateTimeField(auto_now=True) #The moment its updated
+	image = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return self.title
